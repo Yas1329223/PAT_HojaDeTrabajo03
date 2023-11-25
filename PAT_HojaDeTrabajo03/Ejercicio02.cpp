4ee01c58464da7b63dec6756585e0d217805f344
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 15:23:10 -0600

    Update Ejercicio02.cpp

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -2,5 +2,20 @@
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+    std::unordered_map<std::string, std::vector<std::string>> anagramMap;
+    for (const std::string& word : words) 
+    {
+        std::string sortedWord = word;
+        std::sort(sortedWord.begin(), sortedWord.end());
+
+        anagramMap[sortedWord].push_back(word);
+    }
+
+    std::vector<std::vector<std::string>>* result = new std::vector<std::vector<std::string>>;
+
+    for (auto it = anagramMap.begin(); it != anagramMap.end(); ++it) 
+    {
+        result->push_back(it->second);
+    }
+    return result;
 }
