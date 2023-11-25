f0e1d32217c1e108ac9ebfa188593fb37fdfe188
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-11-25 15:37:03 -0600

    Update Ejercicio03.cpp

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -2,5 +2,25 @@
 
 Node<int>* Ejercicio03::copyList(Node<int>* head)
 {
-    return nullptr;
+    if (!head) 
+    {
+        return nullptr;
+    }
+    std::unordered_map<Node<int>*, Node<int>*> nodeMap;
+
+    Node<int>* originalNode = head;
+    while (originalNode) 
+    {
+        nodeMap[originalNode] = new Node<int>{ originalNode->value, nullptr, nullptr };
+        originalNode = originalNode->next;
+    }
+
+    originalNode = head;
+    while (originalNode) 
+    {
+        nodeMap[originalNode]->next = nodeMap[originalNode->next];
+        nodeMap[originalNode]->random = nodeMap[originalNode->random];
+        originalNode = originalNode->next;
+    }
+    return nodeMap[head];
 }
